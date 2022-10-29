bfa0a6501a48cccd7bd6c9d0f2669e74b95704a3
babul <babul.cu.cse35@gmail.com>
2022-10-29 09:13:40 +0600

    java string comparisons

---
+++ src/javaBasic/SubstringComparisons.java
@@ -0,0 +1,31 @@
+package javaBasic;
+
+import java.util.*;
+
+public class SubstringComparisons {
+    public static String getSmallestAndLargest(String s, int k) {
+        String smallest = s.substring(0, k);
+        String largest = "";
+
+        // Complete the function
+        for (int i = 0; i <= s.length() - k; i++) {
+            String substring = s.substring(i, i + k);
+            if ((substring.compareTo(smallest)) <= 0) {
+                smallest = substring;
+            }
+            if ((substring.compareTo(largest)) >= 0) {
+                largest = substring;
+            }
+        }
+        return smallest + "\n" + largest;
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.next();
+        int k = scan.nextInt();
+        scan.close();
+
+        System.out.println(getSmallestAndLargest(s, k));
+    }
+}
